c86514793114cc8b8e83c1ba4909d40970740d1b
protomens <sieve@eratosthen.es>
2021-07-06 19:17:46 -0400

    Push Notifications - Monerado v0.5.0 (Maximilien Robespierre)

--- app/src/main/java/cc/symplectic/monerado/MainActivity.java
+++ app/src/main/java/cc/symplectic/monerado/MainActivity.java
@@ -1,5 +1,8 @@
 package cc.symplectic.monerado;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +17,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 
 import cc.symplectic.monerado.databinding.ActivityMainBinding;
 import cc.symplectic.monerado.fragmets.MenuFragment;
@@ -23,6 +28,7 @@ import cc.symplectic.monerado.fragmets.StartupFragment;
 import java.util.ArrayList;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -67,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
         // Begin the transaction
 
+        createNotificationChannel();
+        createWorker();
+
         MainMenu.add("General & Payment Info");
         MainMenu.add("Worker Stats");
         MainMenu.add("Block Payments");
@@ -80,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
          */
 
         File file = new File(getApplicationContext().getFilesDir().getPath() + "/remrigs.json");
+        File idfile = new File(getApplicationContext().getFilesDir().getPath() + "/identifiers.mo");
         if (file.exists()) {
             Log.d("MA", "remrigs.json EXISTS!");
             //ReadWriteGUID workerRemrigs = new ReadWriteGUID("remrigs.json");
@@ -90,6 +100,15 @@ public class MainActivity extends AppCompatActivity {
             ReadWriteGUID workerRemrigs = new ReadWriteGUID("remrigs.json");
             workerRemrigs.writeToFile("{}", getApplicationContext());
             //remrigs = workerRemrigs.readFromFile(getApplicationContext());
+        }
+
+        if (idfile.exists()) {
+            Log.d("MA", "Identifiers file EXISTS!");
+        }
+        else {
+            Log.d("MA", "Identifiers FILE DOES NOT EXIST! Creating...");
+            ReadWriteGUID identifiersFile = new ReadWriteGUID("identifiers.mo");
+            identifiersFile.writeToFile("[]", getApplicationContext());
         }
 
         // No need to read the file here.
@@ -143,6 +162,33 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.monerado_main_frame, fragment)
                 .addToBackStack(null)
                 .commit();
+
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(String.valueOf(R.string.CHANNEL_ID), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void createWorker() {
+        PeriodicWorkRequest MinerIDs =
+                new PeriodicWorkRequest.Builder(MinerIdentifiersWorker.class, 15, TimeUnit.MINUTES)
+                        // Constraints
+                        .build();
+
+        WorkManager.getInstance(this).enqueue(MinerIDs);
+
 
     }
 
